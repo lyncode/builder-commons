156f933cdca0ad0bfdc0ec186a1fd629e19c45f0
João Melo <jmelo@lyncode.com>
2013-12-28 03:21:03 +0000

    Using guava base function to transform List Builder.

--- src/main/java/com/lyncode/builder/ListBuilder.java
+++ src/main/java/com/lyncode/builder/ListBuilder.java
@@ -1,8 +1,13 @@
 package com.lyncode.builder;
 
+import com.google.common.base.Function;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+
+import static com.google.common.collect.Collections2.transform;
+import static com.google.common.collect.Lists.newArrayList;
 
 public class ListBuilder<T> implements Builder<List<T>> {
     private List<T> list;
@@ -26,14 +31,7 @@ public class ListBuilder<T> implements Builder<List<T>> {
         return list;
     }
 
-    public <E> List<E> build(Transformer<T, E> transformer) {
-        List<E> transformed = new ArrayList<E>();
-        for (T elem : this.list)
-            transformed.add(transformer.transform(elem));
-        return transformed;
-    }
-
-    public abstract static class Transformer<T, E> {
-        public abstract E transform(T elem);
+    public <E> List<E> build(Function<T, E> transformer) {
+        return newArrayList(transform(list, transformer));
     }
 }
